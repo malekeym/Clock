15864935d23d8fd64487c785d7f7df32e3476e81
Mehdi Maleki <malekeym@gmail.com>
2021-04-06 21:20:34 +0430

    use closure and make a clock component

--- src/Clock.js
+++ src/Clock.js
@@ -1,62 +1,70 @@
-var canvas = document.querySelector("#Clock");
-var ctx = canvas.getContext("2d");
-ctx.translate(250, 250);
-var gradient = ctx.createRadialGradient(0, 0, 150, 0, 0, 500);
-gradient.addColorStop(0.1, "#2d3436");
-gradient.addColorStop(0.2, "#ffb142");
-function drawClock() {
-  ctx.fillStyle = gradient;
-  ctx.fillRect(-250, -250, 500, 500);
-  let radius = 250 * 0.9;
-  ctx.fillStyle = "#bdc3c7";
-  ctx.font = "30px Arial";
-  ctx.font = radius * 0.15 + "px Vazir";
-  ctx.textBaseline = "middle";
-  ctx.textAlign = "center";
-  for (let num = 12; num > 0; num--) {
-    ctx.fillText(num.toString(), 0, -150);
-    ctx.rotate((-30 * Math.PI) / 180);
+import "./Clock.css"
+function initialClock(){
+  var canvas = document.querySelector("#canvasClock");
+  var ctx = canvas.getContext("2d");
+  const height = 480/2;
+  console.log(height)
+  ctx.translate(height, height);
+  let radius = height * 0.6;
+  var gradient = ctx.createRadialGradient(0, 0, radius, 0, 0, height*2);
+  gradient.addColorStop(0.1, "#2d3436");
+  gradient.addColorStop(0.2, "#ffb142");
+  function drawClock(ctx) {
+    ctx.fillStyle = gradient;
+    ctx.fillRect(-height, -height, height*2, height*2);
+    ctx.fillStyle = "#bdc3c7";
+    ctx.font = "30px Arial";
+    ctx.font = radius * 0.15 + "px Vazir";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    for (let num = 12; num > 0; num--) {
+      ctx.fillText(num.toString(), 0, -radius);
+      ctx.rotate((-30 * Math.PI) / 180);
+    }
   }
-}
-function drawTime(ctx, radius) {
-  let now = new Date();
-  let hour = now.getHours();
-  let minute = now.getMinutes();
-  let second = now.getSeconds();
-  hour = hour % 12;
-  hour = (hour * Math.PI) / 6 + (minute * Math.PI) / (6 * 60);
-  drawHand(ctx, hour, radius * 0.5, radius * 0.07);
-  minute = minute * (Math.PI / 30);
-  drawHand(ctx, minute, radius * 0.8, radius * 0.07);
-  second = second * (Math.PI / 30);
-  drawHand(ctx, second, radius * 0.9, radius * 0.02);
-  ctx.beginPath();
-  ctx.arc(0, 0, 12.5, 0, 2 * Math.PI);
-  ctx.fillStyle = "#aaa69d";
-  ctx.fill();
+  function drawTime(ctx, radius) {
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+    hour = hour % 12;
+    hour = (hour * Math.PI) / 6 + (minute * Math.PI) / (6 * 60);
+    drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+    minute = minute * (Math.PI / 30);
+    drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+    second = second * (Math.PI / 30);
+    drawHand(ctx, second, radius * 0.9, radius * 0.02);
+    ctx.beginPath();
+    ctx.arc(0, 0, 12.5, 0, 2 * Math.PI);
+    ctx.fillStyle = "#aaa69d";
+    ctx.fill();
+  }
+  
+  function drawHand(ctx, pos, length, width) {
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0, 0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.strokeStyle = "#f1c40f";
+    ctx.stroke();
+    ctx.rotate(-pos);
+  }
+  return {drawClock, drawTime, ctx}
 }
 
-function drawHand(ctx, pos, length, width) {
-  ctx.beginPath();
-  ctx.lineWidth = width;
-  ctx.lineCap = "round";
-  ctx.moveTo(0, 0);
-  ctx.rotate(pos);
-  ctx.lineTo(0, -length);
-  ctx.strokeStyle = "#f1c40f";
-  ctx.stroke();
-  ctx.rotate(-pos);
-}
 function clockHandler(){
-  console.log("running")
   function startTime() {
+    const {drawClock,drawTime, ctx} = initialClock()
     setInterval(() => {
-      drawClock();
+      drawClock(ctx);
       drawTime(ctx, 150);
     }, 1000);
   }
   return(
-    <div>
+    <div id="Clock">
+      <canvas id="canvasClock" width="500" height="500"></canvas>
       <button onClick={startTime} >start clock</button>
     </div>
   )
