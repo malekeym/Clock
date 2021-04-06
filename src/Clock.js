3ba7fe59ae9db0ead3be6cfa623dd9b381ce5a6d
Mehdi Maleki <malekeym@gmail.com>
2021-04-06 21:59:24 +0430

    beutify Clock

--- src/Clock.js
+++ src/Clock.js
@@ -7,12 +7,12 @@ function initialClock(){
   ctx.translate(height, height);
   let radius = height * 0.6;
   var gradient = ctx.createRadialGradient(0, 0, radius, 0, 0, height*2);
-  gradient.addColorStop(0.1, "#2d3436");
-  gradient.addColorStop(0.2, "#ffb142");
+  gradient.addColorStop(0.2, "#2d343600");
+  gradient.addColorStop(0.1, "#ffb142");
   function drawClock(ctx) {
     ctx.fillStyle = gradient;
     ctx.fillRect(-height, -height, height*2, height*2);
-    ctx.fillStyle = "#bdc3c7";
+    ctx.fillStyle = "#130f40";
     ctx.font = "30px Arial";
     ctx.font = radius * 0.15 + "px Vazir";
     ctx.textBaseline = "middle";
@@ -36,7 +36,7 @@ function initialClock(){
     drawHand(ctx, second, radius * 0.9, radius * 0.02);
     ctx.beginPath();
     ctx.arc(0, 0, 12.5, 0, 2 * Math.PI);
-    ctx.fillStyle = "#aaa69d";
+    ctx.fillStyle = "#2C5364";
     ctx.fill();
   }
   
@@ -47,7 +47,7 @@ function initialClock(){
     ctx.moveTo(0, 0);
     ctx.rotate(pos);
     ctx.lineTo(0, -length);
-    ctx.strokeStyle = "#f1c40f";
+    ctx.strokeStyle = "#0F2027";
     ctx.stroke();
     ctx.rotate(-pos);
   }
@@ -65,7 +65,7 @@ function clockHandler(){
   return(
     <div id="Clock">
       <canvas id="canvasClock" width="500" height="500"></canvas>
-      <button onClick={startTime} >start clock</button>
+      <button className="ui yellow button"onClick={startTime} >start clock</button>
     </div>
   )
 }
