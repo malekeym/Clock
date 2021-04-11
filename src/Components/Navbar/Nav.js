38faf5241162a364c5a7d792435a3bb74e77771e
Mehdi Maleki <malekeym@gmail.com>
2021-04-11 12:40:30 +0430

    add className -active- in better way

--- src/Components/Navbar/Nav.js
+++ src/Components/Navbar/Nav.js
@@ -3,15 +3,15 @@ import "./Nav.css";
 function Nav({setShow, show}) {
   return (
     <div id="nav" className="ui inverted menu secondary vertical pointing menu">
-      <a className={`${show==="Clock"?"active":null} item`} onClick={()=>setShow("Clock")}>
+      <a className={`${show==="Clock" && "active"} item`} onClick={()=>setShow("Clock")}>
         <i className="clock icon"></i>
         Clock
       </a>
-      <a className={`${show==="Timer"?"active":null} item`} onClick={()=>setShow("Timer")}>
+      <a className={`${show==="Timer" && "active"} item`} onClick={()=>setShow("Timer")}>
         <i className="hourglass icon"></i>
         Timer
       </a>
-      <a className={`${show==="Stopwatch"?"active":null} item`} onClick={()=>setShow("Stopwatch")}>
+      <a className={`${show==="Stopwatch" && "active"} item`} onClick={()=>setShow("Stopwatch")}>
         <i className="stopwatch icon"></i>
         Stopwatch
       </a>
